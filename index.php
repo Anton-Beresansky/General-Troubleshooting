5d7795d92026acadab6a6ee80cfe1850ec09fc83
Anton Berezianskyi <A.Beresansky@gmail.com>
2016-01-26 22:57:02 +0200

    S

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include_once "config.php";
+
+$MySQLConnection = mysql_connect($servername, $username, $password);
+    $MySQLSelectedDB = mysql_select_db($dbname, $MySQLConnection);
+    mysql_query("SET $dbname utf8");
+
+    $MySQLRecordSet = mysql_query('SELECT keyword FROM main');
+    while($Result = mysql_fetch_assoc($MySQLRecordSet))
+    {
+        foreach($Result as $k => $val)
+        {
+            echo "Name: " . $val;
+        }
+        echo "<br>";
+    }
+
+?>
